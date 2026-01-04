9502b967befb5fba635e28c33d329e3c23cfd5c5
holistudio <daniel.bin.lu@gmail.com>
2026-01-04 17:30:11 -0500

    Convert `LapTime` from Timedelta to float seconds

--- src/export_data.py
+++ src/export_data.py
@@ -11,7 +11,7 @@ laps = session.laps
 
 
 # Inspect columns
-print(f'Lap Data Columns: {laps.columns}')
+print(f'\nLap Data Columns: {laps.columns}')
 
 # Inspect row count
 print(f'Number of rows: {len(laps)}')
@@ -20,8 +20,13 @@ print(f'Number of rows: {len(laps)}')
 drivers = laps['Driver'].unique()
 print(f'Drivers: {drivers}\n')
 
+# Convert `LapTime` from Timedelta to float seconds
+laps['LapTime'] = laps['LapTime'].dt.total_seconds()
+
+# Check for NaT values in LapTime
 if laps['LapTime'].isna().any():
     n_nat = laps['LapTime'].isna().sum()
     print(f'Number of NaT Lap Times:{n_nat}')
 else:
     print('All Lap Times OK')
+
